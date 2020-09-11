e9b0238c4eca98a97753af173b6727e1ee232ec2
augusito <kaugustus15@gmail.com>
2020-09-11 13:26:05 +0300

    correct typo on db connection variable

--- src/models/index.js
+++ src/models/index.js
@@ -10,8 +10,8 @@ const db = {};
 
 let sequelize;
 
-if (config.use_env_constiable) {
-  sequelize = new Sequelize(process.env[config.use_env_constiable], config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   console.log('here!');
   sequelize = new Sequelize(
